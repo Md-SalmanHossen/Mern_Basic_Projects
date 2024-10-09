af5ed510793415eefb7f0c0d75de1d80b9dbd866
SalmanHossen <hossainsalman873@gmail.com>
2024-10-09 14:17:30 +0600

    add all files and folder

--- 01NoteApp/backend/app.js
+++ 01NoteApp/backend/app.js
@@ -2,10 +2,11 @@
 
 const express=require('express');
 const cors=require('cors');
-
+const noteRoutes=require('./routes/noteRoutes');
+const connectDB=require('./config/db')
 const app =express();
-
 app.use(cors());
-app.use(express.json({limit:"10kb"}));
+app.use(express.json());
+
 
 module.exports=app;
